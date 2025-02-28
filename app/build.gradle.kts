35571ca5b789c26d8bd53469ed78bec85fe6eac5
LyesHamrani <124377714+LyesHamrani@users.noreply.github.com>
2025-02-28 08:20:52 -0500

    Interface de login utilisateur

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,12 +35,14 @@ android {
         jvmTarget = "11"
     }
     buildFeatures {
+        viewBinding = true
         compose = true
     }
 }
 
 dependencies {
 
+    implementation(libs.lottie)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
